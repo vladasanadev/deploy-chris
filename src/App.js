936d65b768e15e237dc8ddc49eaad8607de95bd9
EndijaM <115448772+EndijaM@users.noreply.github.com>
2022-12-20 17:31:25 +0100

    Added axios, useState and some functions for weather display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+  const [update, setUpdate] = useState(false);
+
+  function displayWeather(response) {
+    setUpdate(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    if (city.length > 0) {
+      axios.get(url).then(displayWeather);
+    } else {
+      alert(`Please type a city!`);
+    }
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
       <div className="container">
-        <form action="" className="mb-3">
+        <form action="" className="mb-3" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -13,6 +45,7 @@ function App() {
                 placeholder="Type a city"
                 autocomplete="off"
                 className="form-control"
+                onChange={updateCity}
               />
             </div>
             <div className="col-3">
@@ -79,5 +112,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
